5fdccf9f8e3533add3a9c16f86343e06742f4288
Mohamad Nezhadhendi <m.nezhad@saadat.local>
2021-11-15 16:52:08 +0330

    first Commit,Added Customer Proto(Get Customers)

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using GrpcServer;
+using GrpcServer.Protos;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //var input = new HelloRequest {Name = "Mohammad"};
+            //var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            //var client = new Greeter.GreeterClient(channel);
+            //var reply = await client.SayHelloAsync(input);
+            //Console.WriteLine(reply.Message);
+
+            var channel  = GrpcChannel.ForAddress("https://localhost:5001");
+            var customerClient = new Customer.CustomerClient(channel);
+            var clientinput = new CustomerLookupModel {UserId = 1};
+            var reply = await customerClient.GetCustomerInfoAsync(clientinput);
+
+            Console.WriteLine(reply.FirstName);
+            Console.WriteLine(reply.LastName);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ GrpcServer/Services/CustomerService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+using GrpcServer.Protos;
+using Microsoft.Extensions.Logging;
+using ILogger = Grpc.Core.Logging.ILogger;
+
+namespace GrpcServer.Services
+{
+    public class CustomerService:Customer.CustomerBase
+    {
+        private readonly ILogger<CustomerService> _logger;
+
+        public CustomerService(ILogger<CustomerService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<CustomerModel> GetCustomerInfo(CustomerLookupModel request, ServerCallContext context)
+        {
+            var output = new CustomerModel();
+            switch (request.UserId)
+            {
+                case 1:
+                    output.FirstName = "MohammadHossein";
+                    output.LastName = "Nezhadhendi";
+                    break;
+                case 2:
+                    output.FirstName = "Ahmad";
+                    output.LastName = "Mansouri";
+                    break;
+                case 3:
+                    output.FirstName = "Sepehr";
+                    output.LastName = "Karimi";
+                    break;
+            }
+
+            return Task.FromResult(output);
+        }
+    }
+}
